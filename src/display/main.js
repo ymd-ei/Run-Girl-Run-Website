28da8264b2c2bc0ad43d2635ebd1e9d961806843
ymd-ei <154393715+ymd-ei@users.noreply.github.com>
2026-05-04 20:39:52 -0400

    cursor: reduce availability badge display time to 20s

--- src/display/main.js
+++ src/display/main.js
@@ -102,7 +102,7 @@ function updateCursorWorkBadge(x, y, visible = true) {
       curWorkBadgeDismissTimer = setTimeout(() => {
         curWorkBadgeDismissed = true;
         badge.classList.remove('is-visible');
-      }, 45000);
+      }, 20000);
     }
   }
 }
